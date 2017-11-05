a108883cc53b2fc79b95fffc4d4e747f8caccc28
Fredrik Østrem <frx.apps@gmail.com>
2017-11-05 18:17:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# cpp-library
+
+A small library of various C++ classes and functions.
+
+## Requirements
+
+Requires **C++11** and **Boost 1.64.0 with `Boost.Context`**.
+
+## Usage
+
+### Generators
+
+```c++
+#include <frxstrem/generator.hh>
+using namespace frxstrem::generator;
+
+/**
+ * Simple generator.
+**/
+sequence<int> range(int start, int end) {
+  // wrap inside a callable void(yield_t<int>)
+  // which can be converted to sequence<int>
+  return [=] (yield_t<int> yield) {
+    for(int i = start; i < end; i++) {
+      // return next value of sequence
+      yield(i);
+    }
+  };
+}
+
+/**
+ * Generator of pairs
+**/
+sequence<std::pair<int, int>> fib(int N) {
+  return [=] (yield_t<std::pair<int, int>> yield) {
+    int a = -1;
+    int b = 1;
+
+    for(int i = 0; i < N; i++) {
+      int c = a + b;
+      a = b;
+      b = c;
+
+      // yield can also be called with constructor arguments
+      // of yielded type
+      yield(i, c);
+    }
+  };
+}
+
+/**
+ * Yielding vectors
+**/
+sequence<int> vector2sequence(const std::vector<int> &vec) {
+  return [=] (yield_t<int> yield) {
+    // yield* can be used to yield elements of any iterable
+    // of a type convertible to the sequence type
+    // (including other sequences)
+    yield*(vec);
+  };
+}
+
+/**
+ * void sequences
+**/
+sequence<void> voidseq(int N) {
+  return [=] (yield_t<void> yield) {
+    // yield can be used without arguments for void sequences
+    yield();
+  };
+}
+
+
+int main() {
+  // use sequence as an iterable in a for-loop
+  for(auto x : range(0, 5))
+    std::cout << x << " ";
+  std::cout << std::endl;
+  // output: 0 1 2 3 4
+
+
+  // or use .begin() and .end() manually
+  auto seq = range(5, 10);
+  for(auto it = seq.begin(); it != seq.end(); it++)
+    std::cout << *it << " ";
+  std::cout << std::endl;
+  // output: 5 6 7 8 9
+
+
+  // use void sequences
+  for(auto _ : voidseq(5)) {
+    // ...
+  }
+}
+```
---
+++ include/frxstrem/generator.hh
@@ -0,0 +1,161 @@
+#ifndef FRXSTREM_GENERATOR_HH
+#define FRXSTREM_GENERATOR_HH
+
+#include <functional>
+#include <memory>
+#include <utility>
+#include <type_traits>
+#include <stdexcept>
+#include <boost/context/all.hpp>
+
+namespace frxstrem {
+namespace generator {
+
+// forward declarations
+template <typename> class yield_t;
+template <typename> class sequence;
+
+// empty class
+struct empty_t final {
+  constexpr operator bool() const { return false; }
+};
+
+template <typename T>
+class yield_t {
+public:
+  using value_t = typename sequence<T>::value_t;
+
+private:
+  sequence<T> &seq;
+  boost::context::continuation &sink;
+
+  yield_t(sequence<T> &seq, boost::context::continuation &sink);
+
+public:
+  value_t &state();
+  const value_t &state() const;
+
+  template <typename... Args>
+  void operator ()(Args&&... args) { this->operator()(value_t(std::forward<Args>(args)...)); }
+
+  void operator ()(const value_t &value);
+  void operator ()(value_t &&value);
+
+  template <typename Iterable>
+  void operator *(Iterable &&x) {
+    for(T value : x)
+      this->operator()(value);
+  }
+
+  template <typename> friend class sequence;
+};
+
+template <typename T>
+class sequence {
+public:
+  class iterator;
+  using callback_t = std::function<void(yield_t<T>)>;
+  using value_t = typename std::conditional<std::is_void<T>::value, empty_t, T>::type;
+
+private:
+  std::shared_ptr<boost::context::continuation> sourceptr = nullptr;
+  value_t state;
+  bool begun = false, done = false;
+
+  value_t &get() { return state; }
+  const value_t &get() const { return state; }
+
+  void next() { *sourceptr = sourceptr->resume(); }
+
+public:
+  sequence(callback_t cb);
+
+  template <typename Callback>
+  sequence(Callback cb) : sequence(callback_t(cb)) { }
+
+  iterator begin() {
+    if(!begun)
+      next();
+    return iterator(*this, done);
+  }
+  iterator end() { return iterator(*this, true); }
+
+  class iterator {
+  private:
+    sequence &seq;
+    bool end;
+
+    iterator(sequence &seq, bool end) : seq(seq), end(end) { }
+
+  public:
+    value_t &operator *() {
+      if(end) {
+        throw std::runtime_error("Cannot dereference end iterator (frxstrem::generator::sequence::iterator)");
+      }
+
+      return seq.get();
+    }
+
+    iterator &operator ++() {
+      if(end) {
+        throw std::runtime_error("Cannot increment end iterator (frxstrem::generator::sequence::iterator)");
+      }
+
+      seq.next();
+      if(seq.done) {
+        end = true;
+      }
+    }
+
+    bool operator ==(const iterator &it) const {
+      return (&seq == &it.seq && end == it.end);
+    }
+
+    bool operator !=(const iterator &it) const { return !(*this == it); }
+
+    friend class sequence;
+  };
+
+  template <typename> friend class yield_t;
+};
+
+template <typename T>
+yield_t<T>::yield_t(sequence<T> &seq, boost::context::continuation &sink) : seq(seq), sink(sink) { }
+
+template <typename T>
+typename yield_t<T>::value_t &yield_t<T>::state() { return seq.get(); }
+
+template <typename T>
+const typename yield_t<T>::value_t &yield_t<T>::state() const { return seq.get(); }
+
+template <typename T>
+void yield_t<T>::operator ()(const value_t &value) {
+  seq.get() = value;
+  sink = sink.resume();
+}
+
+template <typename T>
+void yield_t<T>::operator ()(value_t &&value) {
+  seq.get() = std::move(value);
+  sink = sink.resume();
+}
+
+template <typename T>
+sequence<T>::sequence(callback_t cb) {
+  sourceptr = std::make_shared<boost::context::continuation>(
+    boost::context::callcc([this, cb] (boost::context::continuation &&sink) {
+      yield_t<T> yield(*this, sink);
+      sink = sink.resume();
+      this->begun = true;
+
+      cb(yield);
+
+      this->done = true;
+      return std::move(sink);
+    })
+  );
+}
+
+}}
+
+#endif // FRXSTREM_GENERATOR_HH
